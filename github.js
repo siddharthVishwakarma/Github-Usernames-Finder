6fc890c18b2d79bd398f28b06415c5461a0f96f3
Siddharth Vishwakarma <sid.sv14@gmail.com>
2021-06-14 21:14:43 +0530

    update

--- github.js
+++ github.js
@@ -1,16 +1,27 @@
 class Github {
-    constructor(){
-        this.client_id = '42a76ecd8f87d8fc1fc4';
-        this.client_secret = 'd1a26c7bcc64fd9c738e705edd24c652c5556440';
-    }
+  constructor() {
+    this.client_id = "42a76ecd8f87d8fc1fc4";
+    this.client_secret = "d1a26c7bcc64fd9c738e705edd24c652c5556440";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
+  }
 
-    async getUser(user){
-        const profileRes = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
 
-        const profile = await profileRes.json();
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
 
-        return {
-            profile
-        }
-    }
+    const profile = await profileResponse.json();
+
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos,
+    };
+  }
 }
